652e12b25adbaa98322d52a3c17a888187be18af
sajithpj <sajithsanju93@gmail.com>
2023-08-01 17:58:09 +0530

    added validation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,76 @@ const btn = document.querySelector(".menu-button");
 const menu = document.querySelector(".mobile-menu");
 const closeNav = document.querySelector(".close-nav");
 const role = document.querySelector("#tag-role");
+const contactFormSubmitBtn = document.querySelector("#submit-contact-form");
 
+// CONTACT FORM
+
+const name = document.querySelector("#name");
+const phone = document.querySelector("#phone");
+const emal = document.querySelector("#emal");
+const message = document.querySelector("#message");
+
+const generateError = (
+  tag = "span",
+  id,
+  className = "error-msg",
+  message = "Required"
+) => {
+  let errorTag = document.createElement(tag);
+  errorTag.className = className;
+  errorTag.id = id;
+  errorTag.innerText = message;
+  return errorTag;
+};
+
+const removeError = (event, inputId) => {
+  let isErrorExist = document.querySelector(`#${inputId}-error`);
+  console.log(inputId, "dfsdfds");
+  console.log(isErrorExist, "dfsdfds");
+  if (isErrorExist) {
+    event.target.classList.remove("error-border");
+    isErrorExist.remove();
+  }
+};
+
+const valdiateForm = (inputs) => {
+  let error = false;
+  inputs.forEach((input) => {
+    let targetInput = input.input;
+    let value = targetInput.value;
+    if (value == "") {
+      // targetInput
+      let isErrorExist = document.querySelector(`#${targetInput.id}-error`);
+      if (!isErrorExist) {
+        error = true;
+        let errorTag = generateError(
+          "span",
+          `${targetInput.id}-error`,
+          "error-msg",
+          input.errorMsg
+        );
+
+        targetInput.classList.add("error-border");
+        targetInput.addEventListener("keydown", (event) => {
+          removeError(event, targetInput.id);
+        });
+        targetInput.parentElement.appendChild(errorTag);
+      }
+    }
+  });
+  return error;
+};
+
+contactFormSubmitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let isValid = valdiateForm([
+    { input: name, errorMsg: "Name is required" },
+    { input: phone, errorMsg: "Phone number is required" },
+    { input: email, errorMsg: "Email is required" },
+    { input: message, errorMsg: "Message is required" },
+  ]);
+  console.log(isValid);
+});
 closeNav.addEventListener("click", () => {
   menu.classList.add("hidden");
 });
@@ -10,78 +79,3 @@ btn.addEventListener("click", () => {
   menu.classList.remove("hidden");
   menu.classList.add("load-nav");
 });
-
-// var TxtType = function (el, toRotate, period) {
-//   this.toRotate = toRotate;
-//   this.el = el;
-//   this.loopNum = 0;
-//   this.period = parseInt(period, 10) || 2000;
-//   this.txt = "";
-//   this.tick();
-//   this.isDeleting = false;
-// };
-
-// TxtType.prototype.tick = function () {
-//   var i = this.loopNum % this.toRotate.length;
-//   var fullTxt = this.toRotate[i];
-
-//   if (this.isDeleting) {
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//   } else {
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//   }
-
-//   this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
-//   var that = this;
-//   var delta = 200 - Math.random() * 100;
-
-//   if (this.isDeleting) {
-//     delta /= 2;
-//   }
-
-//   if (!this.isDeleting && this.txt === fullTxt) {
-//     delta = this.period;
-//     this.isDeleting = true;
-//   } else if (this.isDeleting && this.txt === "") {
-//     this.isDeleting = false;
-//     this.loopNum++;
-//     delta = 500;
-//   }
-
-//   setTimeout(function () {
-//     that.tick();
-//   }, delta);
-// };
-
-// window.onload = function () {
-//   var elements = document.querySelectorAll("#tag-role");
-//   for (var i = 0; i < elements.length; i++) {
-//     var toRotate = [
-//       "Hi, Im Si.",
-//       "I am Creative.",
-//       "I Love Design.",
-//       "I Love to Develop.",
-//     ];
-//     var period = 2000;
-//     if (toRotate) {
-//       new TxtType(elements[i], toRotate, period);
-//     }
-//   }
-//   // INJECT CSS
-//   var css = document.createElement("style");
-//   css.type = "text/css";
-//   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-//   document.body.appendChild(css);
-// };
-
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-
-// let tmp = arr.forEach((elem, index) => {
-//   return (arr[index] = elem * elem);
-// });
-// let tmp2 = arr.filter((elem) => {
-//   return elem;
-// });
-
-// console.log(tmp2);
